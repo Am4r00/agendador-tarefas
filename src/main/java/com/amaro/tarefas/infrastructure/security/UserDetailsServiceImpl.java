2ef370ef93a07141f5345cff1549763027cbe891
joao amaro <joaovictoramaroalves@gmail.com>
2026-02-10 21:32:07 -0300

    adicionando  URL, e corrigindo imports

--- src/main/java/com/amaro/tarefas/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/amaro/tarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -2,13 +2,9 @@ package com.amaro.tarefas.infrastructure.security;
 
 import com.amaro.tarefas.bussines.dto.UsuarioDTO;
 import com.amaro.tarefas.infrastructure.client.UsuarioClient;
-import com.amaro.usuario.infrastructure.entity.Usuario;
-import com.amaro.usuario.infrastructure.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
